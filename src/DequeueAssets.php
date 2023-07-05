08f11ac1f402e34ed0a1fc71a9f6cde67014753d
David Mondok <3883758+davidmondok@users.noreply.github.com>
2023-07-05 12:14:06 +0200

    setup

--- src/DequeueAssets.php
+++ src/DequeueAssets.php
@@ -6,82 +6,132 @@ namespace Kaiseki\WordPress\DequeueAssets;
 
 use Kaiseki\WordPress\Hook\HookCallbackProviderInterface;
 
-use function is_string;
+use function is_array;
+use function is_bool;
+use function is_callable;
 
 /**
- * @phpstan-type DequeueCallback callable(): bool
+ * @phpstan-type DequeueCondition bool|callable(): bool
  * @phpstan-type DequeueConfig array{
- *      dequeue?: DequeueCallback,
  *      action?: string,
+ *      dequeue?: bool|DequeueCondition,
  *      priority?: int,
  * }
+ * @phpstan-type DequeueCallback callable(string $handle): void
  */
 final class DequeueAssets implements HookCallbackProviderInterface
 {
-    /** @var array<string, array<string, DequeueConfig>> $defaultActionconfigs */
-    public array $defaultActionconfigs;
-    /** @var array<string, array<string, DequeueConfig>> $customActionConfigs */
-    public array $customActionConfigs;
+    /** @var array<string, DequeueCondition|array<string, DequeueConfig>> $defaultHookConfigs */
+    public array $defaultHookConfigs;
+    /** @var array<string, DequeueCondition|array<string, DequeueConfig>> $customHookConfigs */
+    public array $customHookConfigs;
 
     /**
      * @param array<string, DequeueConfig> $scripts
      * @param array<string, DequeueConfig> $styles
      */
     public function __construct(
-        array $scripts,
-        array $styles,
+        private readonly array $scripts,
+        private readonly array $styles,
     ) {
-        $this->processConfigs('scripts', $scripts);
-        $this->processConfigs('styles', $styles);
-    }
-
-    /**
-     * @param string $type
-     * @param array<string, DequeueConfig>  $configs
-     *
-     * @return void
-     */
-    private function processConfigs(string $type, array $configs): void
-    {
-        foreach ($configs as $handle => $config) {
-            if (!isset($config['action'])) {
-                continue;
-            }
-            if (!is_string($config['action'])) {
-                continue;
-            }
-            if ($config['action'] === '') {
-                continue;
-            }
-            $this->customActionConfigs[$type][$handle] = $config;
-        }
     }
 
     public function registerHookCallbacks(): void
     {
-        foreach ($this->customActionConfigs['scripts'] as $handle => $config) {
-            if (!isset($config['action'])) {
-                continue;
-            }
-            add_action($config['action'], fn() => $this->dequeueScript($handle), $config['priority'] ?? 11);
-        }
-        foreach ($this->customActionConfigs['styles'] as $handle => $config) {
-            if (!isset($config['action'])) {
-                continue;
-            }
-            add_action($config['action'], fn() => $this->dequeueStyle($handle), $config['priority'] ?? 11);
-        }
-        add_action('wp_enqueue_scripts', [$this, 'dequeueAssets'], 11);
+        [$defaultScriptDequeues, $customScriptDequeues] = $this->getConfigs($this->scripts);
+        $this->registerDefaultHooks($defaultScriptDequeues, [$this, 'dequeueScript']);
+        $this->registerCustomHooks($customScriptDequeues, [$this, 'dequeueScript']);
+        [$defaultStyleDequeues, $customStyleDequeues] = $this->getConfigs($this->styles);
+        $this->registerDefaultHooks($defaultStyleDequeues, [$this, 'dequeueStyle']);
+        $this->registerCustomHooks($customStyleDequeues, [$this, 'dequeueStyle']);
     }
 
-    public function dequeueAssets(): void
+    /**
+     * @param array<string, DequeueCondition|DequeueConfig>  $configs
+     *
+     * @return array{array<string, DequeueCondition>, array<string, DequeueConfig>}
+     */
+    private function getConfigs(array $configs): array
     {
-        foreach ($this->defaultActionconfigs['scripts'] as $handle => $config) {
-            $this->dequeueScript($handle);
+        $defaultHookConfigs = [];
+        $customHookConfigs = [];
+        foreach ($configs as $handle => $config) {
+            if (is_callable($config) || is_bool($config)) {
+                $defaultHookConfigs[$handle] = $config;
+                continue;
+            }
+            if (!is_array($config) || $config === []) {
+                continue;
+            }
+            $customHookConfigs[$handle] = $config;
         }
-        foreach ($this->defaultActionconfigs['styles'] as $handle => $config) {
-            $this->dequeueStyle($handle);
+        return [$defaultHookConfigs, $customHookConfigs];
+    }
+
+    /**
+     * @param array<string, DequeueCondition> $configs
+     * @param callable(string $handle): void $dequeueCallback
+     *
+     * @return void
+     */
+    private function registerDefaultHooks(array $configs, callable $dequeueCallback): void
+    {
+        foreach ($configs as $handle => $condition) {
+            if (
+                !(is_callable($condition) && $condition())
+                && $condition !== true
+            ) {
+                continue;
+            }
+            add_action(
+                'wp_enqueue_scripts',
+                fn() => $dequeueCallback($handle),
+                11
+            );
         }
+    }
+
+    /**
+     * @param array<string, DequeueConfig> $configs
+     * @param DequeueCallback $dequeueCallback
+     *
+     * @return void
+     */
+    private function registerCustomHooks(array $configs, callable $dequeueCallback): void
+    {
+        foreach ($configs as $handle => $config) {
+            $action = $config['action'] ?? 'wp_enqueue_scripts';
+            $priority = $config['priority'] ?? 11;
+            if (!isset($config['dequeue'])) {
+                $this->registerCustomHook($dequeueCallback, $handle, $action, $priority);
+                continue;
+            }
+            if (is_callable($config['dequeue']) && $config['dequeue']()) {
+                $this->registerCustomHook($dequeueCallback, $handle, $action, $priority);
+                continue;
+            }
+            if ($config['dequeue'] !== true) {
+                continue;
+            }
+            $this->registerCustomHook($dequeueCallback, $handle, $action, $priority);
+        }
+    }
+
+    /**
+     * @param DequeueCallback $dequeueCallback
+     * @param string   $handle
+     * @param string   $action
+     * @param int      $priority
+     *
+     * @return void
+     */
+    private function registerCustomHook(callable $dequeueCallback, string $handle, string $action, int $priority): void
+    {
+        add_action(
+            $action,
+            fn() => $dequeueCallback($handle),
+            $priority
+        );
     }
 
     private function dequeueScript(string $script): void
